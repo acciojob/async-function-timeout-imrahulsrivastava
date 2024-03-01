e9892db660fb85f98d1126d7691794e6ad07d6a7
Rahul Kumar Srivastava <124518238+imrahulsrivastava@users.noreply.github.com>
2024-03-01 18:31:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+const textInput = document.getElementById("text");
+const delayInput = document.getElementById("delay");
+const outputDiv = document.getElementById("output");
+const submitBtn = document.getElementById("btn");
+
+submitBtn.addEventListener("click", async () => {
+  const text = textInput.value;
+  const delay = parseInt(delayInput.value);
+
+  outputDiv.innerText = "";
+
+  if (text.trim() === "" || isNaN(delay) || delay <= 0) {
+    outputDiv.innerText = "Please enter valid text and delay";
+    return;
+  }
+
+  submitBtn.disabled = true;
+  await delayFunction(delay);
+  outputDiv.innerText = text;
+  submitBtn.disabled = false;
+});
+
+function delayFunction(delay) {
+  return new Promise((resolve) => setTimeout(resolve, delay));
+}
